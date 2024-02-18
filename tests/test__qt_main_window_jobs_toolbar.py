19fae8fe298eb310f541f999e9efa03994529416
kosaendre <kosa.endre@outlook.com>
2024-02-18 12:46:41 +0100

    improved test coverage for maintablejos, commit before resuming_jobs refactor

--- tests/test__qt_main_window_jobs_toolbar.py
+++ tests/test__qt_main_window_jobs_toolbar.py
@@ -1,7 +1,22 @@
 import unittest
 from unittest.mock import MagicMock
-from PyQt6.QtWidgets import QApplication, QMainWindow, QTableWidget, QPushButton, QTableWidgetItem
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableWidget,
+    QPushButton,
+    QTableWidgetItem,
+)
 from aoget.view.main_window_jobs import MainWindowJobs
+from aoget.model.dto.job_dto import JobDTO
+from aoget.view.main_window_jobs import (
+    JOB_NAME_IDX,
+    JOB_STATUS_IDX,
+    JOB_THREADS_IDX,
+    JOB_SIZE_IDX,
+    JOB_FILES_IDX,
+    JOB_PROGRESS_IDX,
+)
 
 
 class TestMainWindowJobs(unittest.TestCase):
@@ -57,6 +72,43 @@ class TestMainWindowJobs(unittest.TestCase):
         self.controller_mock.add_thread.assert_called_once_with("Test Job")
         self.main_window_jobs._MainWindowJobs__on_job_threads_minus()
         self.controller_mock.remove_thread.assert_called_once_with("Test Job")
+
+    def test_set_job_at_row_job_completed(self):
+        job = JobDTO(
+            id=-1,
+            name="Test Job",
+            status="Not Running",
+            threads_active=1,
+            threads_allocated=3,
+            total_size_bytes=123123123,
+            downloaded_bytes=123123123,
+            selected_files_count=10,
+            selected_files_with_known_size=10,
+            files_done=10,
+            page_url="http://test.com",
+        )
+
+        self.main_window_jobs.setup_ui()
+        self.window.tblJobs.setRowCount(1)
+        self.main_window_jobs.set_job_at_row(0, job)
+        self.assertEqual(self.window.tblJobs.item(0, JOB_NAME_IDX).text(), "Test Job")
+        self.assertEqual(
+            self.window.tblJobs.item(0, JOB_STATUS_IDX).text(), "Not Running"
+        )
+
+    def test_update_job_toolbar_no_selection(self):
+        self.main_window_jobs.update_job_toolbar()
+        self.assertFalse(self.window.btnJobStart.isEnabled())
+        self.assertFalse(self.window.btnJobStop.isEnabled())
+        self.assertFalse(self.window.btnJobThreadsPlus.isEnabled())
+        self.assertFalse(self.window.btnJobThreadsMinus.isEnabled())
+        self.assertFalse(self.window.btnJobEdit.isEnabled())
+        self.assertFalse(self.window.btnJobRemoveFromList.isEnabled())
+        self.assertFalse(self.window.btnJobRemove.isEnabled())
+        self.assertFalse(self.window.btnJobExport.isEnabled())
+        self.assertFalse(self.window.btnJobImport.isEnabled())
+        self.assertFalse(self.window.btnJobOpenLink.isEnabled())
+        self.assertFalse(self.window.btnJobHealthCheck.isEnabled())
 
     def tearDown(self):
         self.window.close()
