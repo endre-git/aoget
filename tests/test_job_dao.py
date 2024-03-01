3c0dcb020f5ba2aa1a52b251aace136b1886aca2
kosaendre <kosa.endre@outlook.com>
2024-03-01 22:26:19 +0100

    fixed #156

--- tests/test_job_dao.py
+++ tests/test_job_dao.py
@@ -2,7 +2,9 @@ import unittest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from aoget.model.job import Job
+from aoget.model.file_model import FileModel
 from aoget.model.dao.job_dao import JobDAO
+from aoget.model.dao.file_model_dao import FileModelDAO
 
 
 class TestJobDAO(unittest.TestCase):
@@ -16,6 +18,7 @@ class TestJobDAO(unittest.TestCase):
 
         # Create an instance of JobDAO with the testing session
         self.job_dao = JobDAO(self.session)
+        self.file_model_dao = FileModelDAO(self.session)
 
     def tearDown(self):
         # Close the session and dispose of the engine after each test
@@ -73,6 +76,55 @@ class TestJobDAO(unittest.TestCase):
 
         # Assert that the job is deleted
         self.assertIsNone(self.job_dao.get_job_by_id(new_job_id))
+
+    def test_when_job_is_deleted_files_are_also_deleted(self):
+        # Create a job
+        new_job = Job(
+            name='Test Job', page_url='http://example.com', target_folder='/aoget'
+        )
+
+        # Save the job using save_job method
+        self.job_dao.add_job(new_job)
+        self.assertNotEqual(0, new_job.id)
+
+        # Create a file model
+        file_model = FileModel(job=new_job, url='http://example.com/file1.txt')
+        self.file_model_dao.add_file_model(file_model)
+        file_model_id = file_model.id
+        self.assertNotEqual(0, file_model.id)
+
+        # Delete the job by its ID
+        self.job_dao.delete_job_by_id(new_job.id)
+
+        # Assert that the file model is deleted
+        self.assertIsNone(self.file_model_dao.get_file_model_by_id(file_model_id))
+
+    def test_when_job_is_deleted_by_name_files_are_also_deleted(self):
+        # Create a job
+        new_job = Job(
+            name='Test Job', page_url='http://example.com', target_folder='/aoget'
+        )
+
+        # Save the job using save_job method
+        self.job_dao.add_job(new_job)
+        self.assertNotEqual(0, new_job.id)
+        new_job_id = new_job.id
+
+        # Create a file model
+        file_model = FileModel(job=new_job, url='http://example.com/file1.txt')
+        self.file_model_dao.add_file_model(file_model)
+        file_model_id = file_model.id
+        self.assertNotEqual(0, file_model.id)
+        self.assertIsNotNone(self.file_model_dao.get_file_model_by_id(file_model_id))
+
+        # Delete the job by its ID
+        self.job_dao.delete_job_by_name("Test Job")
+
+        # sanity check that the job was indeed deleted
+        self.assertIsNone(self.job_dao.get_job_by_id(new_job_id))
+
+        # Assert that the file model is deleted
+        self.assertIsNone(self.file_model_dao.get_file_model_by_id(file_model_id))
 
     def test_delete_all_jobs(self):
         # Create multiple jobs
